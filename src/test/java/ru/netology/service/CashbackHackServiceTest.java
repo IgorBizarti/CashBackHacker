c14fb13fbbbb7cdfb9c35072936bb9c297cf711f
Igor Byazrov <fbi1978@mail.ru>
2023-03-02 15:05:37 +0300

    new branch junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class CashbackHackServiceTest {
 
@@ -13,19 +13,19 @@ public class CashbackHackServiceTest {
         int amount1 = 500;
         int expected1 = 500;
         int actual1 = service.remain(amount1);
-        Assert.assertEquals(actual1, expected1);
+        Assert.assertEquals( expected1,actual1);
 
         // Test case where amount is equal to boundary
         int amount2 = 1000;
         int expected2 = 0;
         int actual2 = service.remain(amount2);
-        Assert.assertEquals(actual2, expected2);
+        Assert.assertEquals( expected2,actual2);
 
         // Test case where amount is greater than boundary
         int amount3 = 1500;
         int expected3 = 500;
         int actual3 = service.remain(amount3);
-        Assert.assertEquals(actual3, expected3);
+        Assert.assertEquals(expected3,actual3);
     }
 }
 
